2604e5eb7e6d6151dfbb79ddab869af4c10d0f9a
Vladimir Tinica <vltinica@gmail.com>
2023-04-18 20:54:42 -0400

    start of Navbar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import Navbar from "@/scenes/navbar"
+import Navbar from "./scenes/navbar/index"
+
+
+
+function App() {
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar/> 
+    </div>
+  )
+}
+
+export default App
